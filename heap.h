2f38f345eedfe4131ac62a7801b24c15be079264
semic0l0n <79784897+hazretimarlboro@users.noreply.github.com>
2026-02-05 18:14:30 +0300

    Refactor heap.h to improve structure and types

--- heap.h
+++ heap.h
@@ -1,25 +1,51 @@
 #ifndef HEAP_H
 #define HEAP_H
-#include <unistd.h>
 
-typedef struct block_t
-{
-    int size;
-    int free;
-    struct block_t* next;
-    struct block_t* prev;
+#include <stddef.h>
+#include <stdint.h>
 
+// --------------------------------------
+// Alignment for blocks (8 bytes)
+// --------------------------------------
+#define ALIGNMENT 8
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1))
+
+// --------------------------------------
+// Block structure
+// --------------------------------------
+typedef struct block {
+    size_t size;          // Size of the block's data
+    int free;             // 1 if free, 0 if allocated
+    struct block* prev;   // Previous block in the region
+    struct block* next;   // Next block in the region
 } block_t;
 
-typedef struct region_t
-{
-    struct region_t* next;
-    int size;
-    block_t* head;
+// --------------------------------------
+// Memory region structure
+// --------------------------------------
+typedef struct region {
+    size_t size;          // Total size of the region
+    block_t* head;        // Head of the block list
+    struct region* next;  // Next region in the heap
 } region_t;
 
-region_t* create_region(size_t size);
-void* _alloc(size_t size);
-void  _free(void* ptr);
+// --------------------------------------
+// Global heap regions pointer
+// --------------------------------------
+extern region_t* regions;
 
-#endif
+// --------------------------------------
+// Allocator functions
+// --------------------------------------
+void* _alloc(size_t size);      // Allocate memory
+void _free(void* ptr);          // Free memory
+
+// --------------------------------------
+// Helper functions (optional, can be used internally)
+// --------------------------------------
+region_t* create_region(size_t size);      // Create a new memory region
+block_t* find_free_block(region_t* region, size_t size); // Find free block in a region
+void split_block(block_t* block, size_t size);           // Split a block
+void coalesce(block_t* block);                            // Merge adjacent free blocks
+
+#endif // HEAP_H
